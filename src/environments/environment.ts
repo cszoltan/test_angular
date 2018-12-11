15e76030bdc086e455c0baa3e3c97a7b608772af
Csikós Zoltán <csikos.zoltan@vitarex.hu>
2018-12-11 14:08:19 +0100

    yaml

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  clientID: 'cf1b2262-31d1-4fad-9fc5-f4e1bde8f297'  
 };
 
 /*
